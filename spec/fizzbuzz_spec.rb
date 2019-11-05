380466e4a820227b0fb38a4397f2a22ee36c400a
kealanheena <kealanheena@gmail.com>
2019-11-05 14:56:21 +0000

    add if statement for multiples of both 3 & 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
     it'returns buzz when passed multiple of 5' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
+    it'returns fizzbuzz when passed multiple of 5 and a multiple 3' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
 end
